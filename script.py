a6203eb7d386fb95878f2e6be108b5d11d1877bd
venujibhakate <venubhaktpralhad18@navgurukul.org>
2019-05-04 22:36:31 +0530

    we made function for get child name and child content

--- script.py
+++ script.py
@@ -9,13 +9,9 @@ def saralRequest(Url):
     request = requests.get(Url)
     response = request.json()
     return response
-
-
 coursesUrl = saralUrl+"/courses"
 coursesResponse = saralRequest(coursesUrl)
-# pprint.pprint (courses_response)
-
-print("************************************Wel-Come in saral******************************")
+# pprint.pprint (coursesResponse)
 
 listCourses = []
 def coursesFunction():
@@ -36,28 +32,27 @@ selectCourse = int(raw_input("Select The Course:-"))
 selectCourseId = listCourses[selectCourse]
 print(selectCourseId)
 
-
 exerciseUrl = "{0}/{1}/exercises".format(coursesUrl, selectCourseId)
-# request1 = requests.get(exerciseUrl)
-# response1 = request1.json()
-# print response1
 exerciseSlugList = []
+childExercise = []
 def exerciseFunction(exerciseData):
     index = 0
     while(index < len(exerciseData['data'])):
         exercise = exerciseData['data'][index]
         exerciseName = exercise["name"]
+        childExerciseList = exercise["childExercises"]
+        childExercise.append(childExerciseList)
+        # print (childExercise)
         exerciseSlug = exercise["slug"]
         print index + 0,exerciseName,"***",exerciseSlug
         exerciseSlugList.append(exerciseSlug)
         index = index + 1
-#     print exerciseSlugList
-
 exerciseResponse = saralRequest(exerciseUrl)
-exerciseFunction(exerciseResponse)
+exerciseData = exerciseFunction(exerciseResponse)
 
 print "......................................................"
-selectExercise = int(raw_input("Select the Exercise"))
+
+selectExercise = int(raw_input("Select the Exercise :-"))
 selectExerciseId = exerciseSlugList[selectExercise]
 print (selectExerciseId)
 
@@ -68,14 +63,31 @@ def contentAndchildEx(slug):
 slugResponse = saralRequest(slugUrl)
 slugData=contentAndchildEx(slugResponse)
 print slugData
+childSlugList = []
+def childExerciseFun():
+        index = 0
+        while index < len(childExercise[selectExercise]):
+                childExerciseIndex = childExercise[selectExercise][index]
+                childName = childExerciseIndex["name"]
+                childSlug = childExerciseIndex["slug"]
+                childSlugList.append(childSlug)
+                print index,"=", childName
+                index = index + 1
+        return childSlugList
+childExerciseFun()
+
+selectChildEx = int(raw_input("Selsec the ChildEx :-"))
+childExSlug = childSlugList[selectChildEx]
+print (childExSlug)
 
 
 
 
+slugUrl = "{0}/{1}/exercise/getBySlug?slug={2}".format(coursesUrl,selectCourseId,childExSlug)
+def contentChild(childSlug):
+        content = childSlug["content"]
+        return content
+childContent = saralRequest(slugUrl)      
+childExContent = contentChild(childContent)
 
-
-
-
-
-
-
+print childExContent 
